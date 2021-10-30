4b458cce90ea9d5ae35e93551c8fea6f5c209221
dineshbits <2020mt93163@wilp.bits-pilani.ac.in>
2021-10-31 02:29:33 +0530

    final contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -23,4 +23,18 @@ contract Election {
         addCandidate("Ishaan");
         addCandidate("Renuka");
     }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
 }
